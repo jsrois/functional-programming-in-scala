f247d2046cf918b7c62fb050de1962cd25c85c4a
Javi <javi@jsrois.net>
2024-05-05 09:41:38 +0200

    exercise 3.3

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -2,7 +2,9 @@ package chapter3
 
 
 sealed trait List[+A]
+
 case object Nil extends List[Nothing]
+
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
@@ -15,5 +17,15 @@ object List {
   def apply[A](args: A*): List[A] =
     if (args.isEmpty) Nil
     else Cons(args.head, apply(args.tail: _*))
+
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(head, tail) => tail
+  }
+
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Cons (h, l)
+    case Cons(_, lt) => Cons(h, lt)
+  }
 
 }
--- src/test/scala/chapter3/Chapter3Tests.scala
+++ src/test/scala/chapter3/Chapter3Tests.scala
@@ -1,11 +1,11 @@
 package chapter3
 
-import chapter3.List.sum
+import chapter3.List.{setHead, sum, tail}
 import org.scalatest.flatspec.AnyFlatSpec
 
 class Chapter3Tests extends AnyFlatSpec {
 
-  "exercise 3.1" should "calculate returns" in {
+  "exercise 3.1" should "find out what is the right match" in {
 
     val x = List(1, 2, 3, 4, 5) match {
       case Cons(x, Cons(2, Cons(4, _))) => x
@@ -16,6 +16,16 @@ class Chapter3Tests extends AnyFlatSpec {
     }
 
     assert(x == 3)
+  }
+
+  "exercise 3.2" should "implement the tail function" in {
+    assert(tail(List(1, 2, 3, 4, 5)) == List(2, 3, 4, 5))
+  }
+
+  "exercise 3.3" should "set a different head for a list" in {
+
+    assert(setHead(Nil,1) == List(1))
+    assert(setHead(List(2,2,3,4,5),1) == List(1,2,3,4,5))
   }
 
 }
